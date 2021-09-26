01242f07d3fdf01b4b4a38ceebfe1970538d5229
red <red@gmail.com>
2021-09-26 17:41:57 +0700

    fixed equal function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let preButton;
+
 // This function clear screen
 function clearScreen(){
     document.getElementById("result").value = "";
@@ -5,12 +7,14 @@ function clearScreen(){
 
 // This function display values
 function display(value){
+    if (preButton === "=" && "0123456789".includes(value)) clearScreen();
     document.getElementById("result").value += value;
+    preButton = "";
 }
 
 //This function delete the last character
 function deleteChar(){
-    var x = document.getElementById("result").value;
+    let x = document.getElementById("result").value;
     if (!x) return;
     x = x.substr(0, x.length - 1);
     document.getElementById("result").value = x;
@@ -18,7 +22,7 @@ function deleteChar(){
 
 //This function evaluates the expression and return result
 function calculate(){
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
+    const p = document.getElementById("result").value;
+    document.getElementById("result").value = eval(p);
+    preButton = "=";
 }
